780ad23d7e399a87218d85b2a9e84147e82e411d
Yagyansh5 <114283643+Yagyansh5@users.noreply.github.com>
2024-08-04 20:04:22 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayInput() {
+    var name = document.getElementById("name").value;
+    var output = document.getElementById("output");
+    output.innerHTML = "Hello, " + name + "!";
+}
